2c10809d6cbd75cd51f537bdfb2ba5cd977209aa
XunAn0508 <86668662+XunAn0508@users.noreply.github.com>
2023-07-21 17:46:01 +0800

    upload

---
+++ Backend/aspnet-core/src/CompanyWebsite.Core/Configuration/AppSettingProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Abp.Configuration;
+using Abp.Net.Mail;
+
+namespace CompanyWebsite.Configuration
+{
+    public class AppSettingProvider : SettingProvider
+    {
+        public override IEnumerable<SettingDefinition> GetSettingDefinitions(SettingDefinitionProviderContext context)
+        {
+            return new[]
+            {
+                new SettingDefinition(AppSettingNames.UiTheme, "red", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, clientVisibilityProvider: new VisibleSettingClientVisibilityProvider())
+,               new SettingDefinition("CareerRecipientEmailAddresses", "HR@8dge.com.my", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, clientVisibilityProvider: new VisibleSettingClientVisibilityProvider())
+,               new SettingDefinition("ContactUsRecipientEmailAddresses", "info@8dge.com.my", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, clientVisibilityProvider: new VisibleSettingClientVisibilityProvider())
+
+            };
+        }
+    }
+}
